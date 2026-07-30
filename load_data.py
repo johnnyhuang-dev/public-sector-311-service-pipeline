b0047109870455e219229210262e903efc84ec3a
johnnyhuang-dev <huangjohnny22@gmail.com>
2026-07-30 09:29:24 -0400

    fix(pipeline): fixed typo to get blob client

--- load_data.py
+++ load_data.py
@@ -18,16 +18,22 @@ LOCAL_FILE_PATH = project_root / "data" / "urban_service_requests.csv"
 BLOB_NAME = "urban_service_requests.csv"
 
 def upload_data():
+    """
+    Uploads data directly from the local file or the API into the Azure blob container
+    """
     try:
+        # Initialize the Azure blob service 
         blob_service_client = BlobServiceClient(
-             account_url = f"https://{ACCOUNT_NAME}.blob.core.windows.net",
+             account_url=f"https://{ACCOUNT_NAME}.blob.core.windows.net",
              credential=ACCOUNT_KEY
         ) 
 
-        blob_client = blob_service_client.get.blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
+        # Retrieve the blob client 
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
 
         print(f'starting file upload from {LOCAL_FILE_PATH}')
 
+        # Open the local file or API data and upload to the blob while overwriting existing data
         with open(LOCAL_FILE_PATH, 'rb') as data:
             blob_client.upload_blob(
                 data,
